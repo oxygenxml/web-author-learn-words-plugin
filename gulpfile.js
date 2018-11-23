ab5c931c913386c61eea9a876abe787be0d5eeee
andreip1123 <andrei.popa1123@gmail.com>
2018-11-23 12:38:16 +0200

    WA-2479: Use local msgs file for i18n.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var Synci18n = require('sync-i18n');
 var concat = require('gulp-concat');
+var iife = require("gulp-iife");
 var uglify = require('gulp-uglify');
 var uglifyOptions = {
   mangle: {
@@ -16,12 +17,16 @@ var targetLocation = "target";
 gulp.task('prepare-package', ['i18n'], function() {
   return gulp.src(webLocation + '/*.js')
     .pipe(concat('plugin.js'))
+    .pipe(iife({useStrict: false, prependSemicolon: true}))
     .pipe(uglify(uglifyOptions))
     .pipe(gulp.dest(targetLocation));
 });
 
 gulp.task('i18n', function () {
-  Synci18n().generateTranslations();
+  Synci18n({
+    // use this flag only if sure that the plugin will be surrounded by IIFE.
+    useLocalMsgs:true
+  }).generateTranslations();
 });
 
 gulp.task('default', ['prepare-package']);
