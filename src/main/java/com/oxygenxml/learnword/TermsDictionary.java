21e09661842c875aa3f516851136d1c98937216f
andreip1123 <andrei.popa1123@gmail.com>
2019-02-05 16:17:11 +0200

    Fix typo.

--- src/main/java/com/oxygenxml/learnword/TermsDictionary.java
+++ src/main/java/com/oxygenxml/learnword/TermsDictionary.java
@@ -26,7 +26,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.oxygenxml.charpicker.SpecialCharServlet;
 import com.oxygenxml.learnword.xmlmarshal.MarshalDictionary;
 
 import ro.sync.exml.workspace.api.spell.Dictionary;
