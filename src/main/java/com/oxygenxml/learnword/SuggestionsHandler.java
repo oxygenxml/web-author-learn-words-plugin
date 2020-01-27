57ab0b313c038c61096c743834685c9303dadbaf
andreip1123 <andrei.popa1123@gmail.com>
2020-01-27 14:17:30 +0200

    Fix small warnings.

--- src/main/java/com/oxygenxml/learnword/SuggestionsHandler.java
+++ src/main/java/com/oxygenxml/learnword/SuggestionsHandler.java
@@ -77,13 +77,11 @@ public class SuggestionsHandler {
 
     for (int i = 0; i <= x.length(); i++) {
       for (int j = 0; j <= y.length(); j++) {
-        if (i == 0)
+        if (i == 0) {
           dp[i][j] = j;
-
-        else if (j == 0)
+        } else if (j == 0) {
           dp[i][j] = i;
-
-        else {
+        } else {
           dp[i][j] = min(dp[i - 1][j - 1]
               + costOfSubstitution(x.charAt(i - 1), y.charAt(j - 1)),
               dp[i - 1][j] + 1, dp[i][j - 1] + 1);
