c61b817118d2519007d76531a8693b8fbf7c8b3e
andreip1123 <andrei_popa@sync.ro>
2021-06-24 12:21:05 +0300

    Stop mangling properties.

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,18 @@ var Synci18n = require('sync-i18n');
 var concat = require('gulp-concat');
 var iife = require("gulp-iife");
 var uglify = require('gulp-uglify');
-var uglifyOptions = {
-  mangle: {
-    properties: {
-      regex: /_$/
-    }
-  }
-};
 
 var webLocation = 'web';
 var targetLocation = "target";
 gulp.task('i18n', function (done) {
-  Synci18n({
-    // use this flag only if sure that the plugin will be surrounded by IIFE.
-    useLocalMsgs:true
-  }).generateTranslations();
+  Synci18n().generateTranslations();
   done();
 });
 gulp.task('prepare-package', gulp.series('i18n', function() {
   return gulp.src(webLocation + '/*.js')
     .pipe(concat('plugin.js'))
     .pipe(iife({useStrict: false, prependSemicolon: true}))
-    .pipe(uglify(uglifyOptions))
+    .pipe(uglify())
     .pipe(gulp.dest(targetLocation));
 }));
 gulp.task('default', gulp.series('prepare-package'));
